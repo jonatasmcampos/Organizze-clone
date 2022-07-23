b389f815eeb021c249581728f8e2985f1e6df0ee
jonatasmcampos <camposjonatas3@gmail.com>
2022-07-23 11:54:35 -0300

    slide introdutório adicionado

--- app/src/main/java/com/example/organizze/MainActivity.java
+++ app/src/main/java/com/example/organizze/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.organizze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.organizze.activity.CadastroActivity;
+import com.example.organizze.activity.LoginActivity;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        /* setContentView(R.layout.activity_main); */
+
+        /* Remover os botoes de navegação padrão do slide */
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        /* Inserindo os 5 slides introdutórios */
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build());
+        addSlide(new FragmentSlide.Builder() /* Layout de cadastro intro */
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .canGoForward(false)
+                .build());
+    }
+
+    public void btnEntrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+    public void btnCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
