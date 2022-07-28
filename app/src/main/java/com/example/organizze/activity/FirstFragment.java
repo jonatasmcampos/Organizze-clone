440bbf1dfffe5b4aa85f65b2b7d8fb2468203343
jonatasmcampos <camposjonatas3@gmail.com>
2022-07-28 16:32:25 -0300

    calendarView adicionado

--- app/src/main/java/com/example/organizze/activity/FirstFragment.java
+++ app/src/main/java/com/example/organizze/activity/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,26 +12,47 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.organizze.R;
 import com.example.organizze.databinding.FragmentFirstBinding;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private MaterialCalendarView calendarView;
+    private TextView textoSaudacao, textoSaldo;
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-        return binding.getRoot();
 
+        textoSaudacao = binding.textSaudacao;
+        textoSaldo = binding.textSaldo;
+        calendarView = binding.calendarView;
+        configuraCalendarView();
+
+        return binding.getRoot();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void configuraCalendarView(){
+        CharSequence[] meses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",
+                "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        calendarView.setTitleMonths( meses );
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
     }
 
 }
