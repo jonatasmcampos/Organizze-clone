423fd2e0366ceb907dd92106699aa24c578445ec
jonatasmcampos <camposjonatas3@gmail.com>
2022-07-28 22:21:41 -0300

    resumo do saldo ok; deslogando usuario ok.

--- app/src/main/java/com/example/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/example/organizze/activity/PrincipalActivity.java
@@ -3,52 +3,102 @@ package com.example.organizze.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.organizze.databinding.ActivityPrincipalBinding;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.appcompat.widget.Toolbar;
 
+import com.example.organizze.MainActivity;
 import com.example.organizze.R;
+import com.example.organizze.config.ConfiguracaoFirebase;
+import com.example.organizze.helper.Base64Custom;
+import com.example.organizze.model.Usuario;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
+
+import java.text.DecimalFormat;
 
 public class PrincipalActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityPrincipalBinding binding;
+    private MaterialCalendarView calendarView;
+    private TextView textoSaudacao, textoSaldo;
+    private Double despesaTotal = 0.0;
+    private Double receitaTotal = 0.0;
+    private Double resumoUsuario = 0.0;
+
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Organizze");
+        setSupportActionBar(toolbar);
 
-        binding = ActivityPrincipalBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        textoSaudacao = findViewById(R.id.textSaudacao);
+        textoSaldo = findViewById(R.id.textSaldo);
+        calendarView = findViewById(R.id.calendarView);
+        configuraCalendarView();
+        recuperarResumo();
 
-        setSupportActionBar(binding.toolbar);
+    }
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    public void recuperarResumo(){
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child( idUsuario );
 
-        /* binding.fab.setOnClickListener(new View.OnClickListener() {
+        usuarioRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Usuario usuario = snapshot.getValue( Usuario.class );
+                despesaTotal = usuario.getDespesaTotal();
+                receitaTotal = usuario.getReceitaTotal();
+                resumoUsuario = receitaTotal - despesaTotal;
+
+                DecimalFormat decimalFormat = new DecimalFormat("0.##");
+                String resultadoFormatado = decimalFormat.format( resumoUsuario );
+                textoSaudacao.setText( "Olá, " + usuario.getNome() + "." );
+                textoSaldo.setText( "R$ " + resultadoFormatado );
             }
-        }); */
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                autenticacao.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void adicionarDespesa(View view){
@@ -57,5 +107,18 @@ public class PrincipalActivity extends AppCompatActivity {
 
     public void adicionarReceita(View view){
         startActivity(new Intent(this, ReceitasActivity.class));
+    }
+
+    public void configuraCalendarView(){
+        CharSequence[] meses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",
+                "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        calendarView.setTitleMonths( meses );
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/organizze/activity/ReceitasActivity.java
+++ app/src/main/java/com/example/organizze/activity/ReceitasActivity.java
@@ -61,6 +61,7 @@ public class ReceitasActivity extends AppCompatActivity {
             atualizarReceita( receitaAtualizada );
 
             movimentacao.salvar( data );
+            finish();
         }
     }
 
